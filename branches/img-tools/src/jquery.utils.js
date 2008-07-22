61e3d15a6eb5413688d7c9c5382055c70c3ed4ae
hhh333 <hhh333@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2008-07-22 20:00:46 +0000

    - created experimental img tools branch

---
+++ branches/img-tools/src/jquery.utils.js
@@ -0,0 +1,48 @@
+/*
+  jQuery utils - 0.1
+  http://code.google.com/p/jquery-utils/
+
+  (c) Maxime Haineault <haineault@gmail.com> 
+  http://haineault.com
+
+  MIT License (http://www.opensource.org/licenses/mit-license.php
+
+*/
+
+(function($){
+
+    // I use the ui widget api for controls
+    $.ctrl = { plugin: $.ui.plugin };
+
+	$.extend({ 
+		isRegExp: function(o) {
+			return o.constructor.toString().indexOf('RegExp()') != -1;
+		},
+		isArray: function(o) {
+			return o.constructor.toString().indexOf('Array()') != -1;
+		},
+		toCurrency: function() {
+			o = parseFloat(this, 10).toFixed(2);
+			return (o=='NaN') ? '0.00' : o;
+		}
+	});
+
+	$.extend($.fn, { 
+        selectRange: function(start, end) {
+            // use only the first one since only one input can be focused
+            if ($(this).get(0).createTextRange) {
+                var range = $(this).get(0).createTextRange();
+                range.collapse(true);
+                range.moveEnd('character',   end);
+                range.moveStart('character', start);
+                range.select();
+            }
+            else if ($(this).get(0).setSelectionRange) {
+                $(this).bind('focus', function(e){
+                    e.preventDefault();
+                }).get(0).setSelectionRange(start, end);
+            }
+            return $(this);
+        }
+	});
+})(jQuery);
