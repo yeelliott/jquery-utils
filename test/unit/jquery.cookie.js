11b0082dde0a4c7602e00fafa529f4b43254f3dd
haineault <haineault@42e6dbca-fe45-0410-8bc7-9300d0be77b8>
2010-03-10 19:41:29 +0000

    Starting from scratch ..

--- test/unit/jquery.cookie.js
+++
@@ -1,8 +0,0 @@
-module('jquery.cookie.js');
-test('Set/unset tests', function() {
-    expect(2);
-    $.cookie('test', 'abc');
-    equals($.cookie('test'), 'abc', 'Set cookie');
-    $.cookie('test', false);
-    equals($.cookie('test'), 'false', 'Unset cookie');
-});
